c1aab734fa72546cfc2f41eb70ec8f986ef1fb5b
Jim Riddle <jimriddle0@gmail.com>
2022-07-18 13:23:45 -0500

    included validation of email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :user_parties
   has_many :parties, through: :user_parties
   validates_presence_of :name
+  validates_presence_of :email
   validates_presence_of :password_digest
   validates_uniqueness_of :email
   has_secure_password

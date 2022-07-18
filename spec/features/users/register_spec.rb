7ae0b44ff4dc4904376ee89052dbc642efd7eadb
Jim Riddle <jimriddle0@gmail.com>
2022-07-18 13:02:07 -0500

    flushed out register user spec with pws

--- spec/features/users/register_spec.rb
+++ spec/features/users/register_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe 'User Registration Page' do
     visit register_path
     fill_in 'name', with: 'Jamar'
     fill_in 'email', with: 'Jamar@sucks.com'
+    fill_in 'password', with: 'password123'
+    fill_in 'password_confirmation', with: 'password123'
     click_on 'Create User'
     expect(current_path).to eq(user_path(User.last.id))
   end

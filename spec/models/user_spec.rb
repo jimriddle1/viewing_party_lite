9c514c3b4030b6aa03b9074a51df0076d9a2e340
Jim Riddle <jimriddle0@gmail.com>
2022-07-18 12:52:53 -0500

    flushed out model test for user story 1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,17 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many(:user_parties) }
     it { should have_many(:parties).through(:user_parties) }
+  end
+
+  describe 'passwords' do
     it { should validate_uniqueness_of(:email)}
+    it { should validate_presence_of(:password_digest)}
+    it { should have_secure_password}
+    it 'has the right attrs for a new user' do
+      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+      # binding.pry
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+    end
   end
 end
